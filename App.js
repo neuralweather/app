33bdb83a4e85c06bdd18ac21402d4127d09095dd
1NO0B <timon.ben.gaertner@gmail.com>
2021-11-08 14:22:44 +0100

    add responsiveness

--- App.js
+++ App.js
@@ -1,43 +1,43 @@
 import React, { useEffect } from "react";
 import { View, ScrollView, Image, StyleSheet, Text } from "react-native";
-
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 async function getData(path) {
-    const response = await fetch("https://reactnative.dev/movies.json" + path);
-    const json = await response.json();
-    return json;
+  const response = await fetch("https://reactnative.dev/movies.json" + path);
+  const json = await response.json();
+  return json;
 }
 
 const heroBgImgFile = "cloudy-rainy.png";
 
-const HeroBg = () => {
-    return (
-        <View>
-            <Image style={styles.heroBgImg} source={require("./images/"+heroBgImgFile)} />
-            <Text>hallo</Text>
-        </View>
-    );
-};
-
 const App = () => {
-    return (
-        <ScrollView style={{backgroundColor: '#809EA1'}}>
-            <View style={{ height: 1000, flex: 1, alignItems: 'center'}}>
-                <HeroBg/>
-            </View>
-        </ScrollView>
-    );
+  return (
+    <ScrollView style={{backgroundColor: "#809EA1" }}>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <Image
+        style={styles.heroBgImg}
+        source={require("./images/" + heroBgImgFile)}
+      />
+      </View>
+    </ScrollView>
+    
+  );
 };
 
 async function a() {
-    var answer = await getData("");
-    console.log(answer);
+  var answer = await getData("");
+  console.log(answer);
 }
 a();
 
 const styles = StyleSheet.create({
-    heroBgImg: {
-        height: '50%',
-        resizeMode: 'contain'
-    },
+  heroBgImg: {
+    width: wp('50%'),
+    height: wp('50%'),
+
+    resizeMode: "contain",
+  },
 });
 export default App;
