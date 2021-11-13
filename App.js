cdb4af9f929129da92e286e075017a82b26ff886
1NO0B <timon.ben.gaertner@gmail.com>
2021-11-13 16:21:29 +0100

    add diagram

--- App.js
+++ App.js
@@ -4,12 +4,20 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 async function getData(path) {
   const response = await fetch("https://reactnative.dev/movies.json" + path);
   const json = await response.json();
   return json;
 }
+
 const heroImgFile = "cloudy-rainy.png";
 const currentTemp = 22;
 const App = () => {
@@ -50,6 +58,50 @@ const App = () => {
             </View>
           </View>
         </View>
+        <LineChart
+    data={{
+      labels: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={wp('80%')} // from react-native
+    height={220}
+    yAxisLabel=""
+    yAxisSuffix="°"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#809EA1",
+      backgroundGradientFrom: "#809EA1",
+      backgroundGradientTo: "#809E00",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
       </View>
     </ScrollView>
   );
